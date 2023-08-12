044cefba2f82d6505184ca252c08b30b93051bab
“shariqalee” <shariq.ali@pitb.gov.pk>
2023-08-12 12:45:48 +0500

    Email custom validation and thunder client: 47

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 // Call the connectToMongo function
 connectToMongo()
 const app = express()
-const port = 3000
+const port = 5000
 app.use(express.json())
 //Available Routes
 app.use('/api/auth', require('./routes/auth'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,25 +4,34 @@ const router = express.Router()
 const { body, validationResult } = require('express-validator')
 
 
-// Create a User using: POST  "/api/registerUser/". Doesn't require Auth
-router.post("/", [
+// Create a User using: POST  "/api/createUser/". Doesn't require Auth
+router.post("/createUser", [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 8 characters').isLength({ min: 8 })
 
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(401).json({ "error": errors.array() })
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(user => res.json(user))
-        .catch(err => {
-            console.log(err)
-            res.json(err.message)
+], async (req, res) => {
+    try {
+        //If there are errors. Return bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(401).json({ "error": errors.array() })
+        //check whether this user email already exist
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(401).json({ "error": 'Sorry a user with this email already exist' })
+        }
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
         })
+        res.json({ "user": user })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send("some error occurred")
+    }
+
 })
 
 module.exports = router
